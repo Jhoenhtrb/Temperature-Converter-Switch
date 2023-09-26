c1437e937d985090914cfa8677a84d1e429e47e6
Jhoenhtrb <144203695+Jhoenhtrb@users.noreply.github.com>
2023-09-26 20:17:13 +0800

    Add files via upload

--- Activity.java
+++ Activity.java
@@ -1,32 +1,30 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-package activity;
+
+package com.mycompany.activity;
+
 import java.util.Scanner;
+
 /**
  *
- * @author Villagonzalo_CPE
+ * @author Admin
  */
 public class Activity {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
-        System.out.println("Hello! Welcome to Tony's Computing Features");
+        System.out.println("Jhoenhtrb Converter");
         System.out.println("  ");
-        Scanner bogo = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("Please Select Option:");
         System.out.println( "  " + "1. Temperature Converter");
         System.out.println( "  " + "2. Odd/Even Number Checker");
         System.out.println( "  " + "3. Trigonometric Calculator");
         System.out.println( "  " + "4. Polygon Surface Area Calculator");
-        int hehe = bogo.nextInt();
+        int jho = input.nextInt();
 
-        if (hehe == 1) {
+        if (jho == 1) {
             System.out.println("Please select to convert:");
             System.out.println("  ");
             System.out.println("1. Celsius to Farenheight");
@@ -35,63 +33,63 @@ public class Activity {
             System.out.println("4. Kelvin to Farenheight");
             System.out.println("5. Celsius to Kelvin");
             System.out.println("6. Farenheight to Kelvin");
-            int haha = bogo.nextInt();
+            int htrb = input.nextInt();
 
-            switch (haha) {
+            switch (htrb) {
                 case 1:
                     System.out.println("Enter Value to Convert:");
-                    double c = bogo.nextDouble();
+                    double c = input.nextDouble();
                     double f = (c * 9 / 5) + 32;
-                    System.out.println("The Answer is:" + " " + f);
+                    System.out.println(f + " " + "Farenheight");
                     break;
 
                 case 2:
                     System.out.println("Enter Value to Convert:");
-                    double f2 = bogo.nextDouble();
+                    double f2 = input.nextDouble();
                     double c2 = (f2 - 32) * 5 / 9;
                     System.out.println("The Answer is:" + " " + c2);
                     break;
 
                 case 3:
                     System.out.println("Enter Value to Convert:");
-                    double k = bogo.nextDouble();
+                    double k = input.nextDouble();
                     double c3 = k - 273.15;
                     System.out.println("The Answer is:" + " " + c3);
                     break;
 
                 case 4:
                     System.out.println("Enter Value to Convert:");
-                    double k2 = bogo.nextDouble();
+                    double k2 = input.nextDouble();
                     double f3 = (k2 - 273.15) * 9 / 5 + 32;
                     System.out.println("The Answer is:" + " " + f3);
                     break;
 
                 case 5:
                     System.out.println("Enter Value to Convert:");
-                    double c4 = bogo.nextDouble();
+                    double c4 = input.nextDouble();
                     double k3 = c4 + 273.15;
                     System.out.println("The Answer is:" + " " + k3);
                     break;
 
                 case 6:
                     System.out.println("Enter Value to Convert:");
-                    double f4 = bogo.nextDouble();
+                    double f4 = input.nextDouble();
                     double k4 = (f4 - 32) * 5 / 9 + 273.15;
                     System.out.println("The Answer is:" + " " + k4);
                     break;
             }
 
-        } else if (hehe == 2) {
+        } else if (jho == 2) {
 
             System.out.print("Enter a Number: ");
-            int x = bogo.nextInt();
+            int x = input.nextInt();
             if (x % 2 == 0) {
-                System.out.println(x + " is even.");
+                System.out.println(x + " is even numbers.");
             } else {
-                System.out.println(x + " is odd.");
+                System.out.println(x + " is odd numbers.");
             }
 
-        } else if (hehe == 3) {
+        } else if (jho == 3) {
 
             System.out.println("Please choose trigonometric functions:");
             System.out.println("  ");
@@ -99,30 +97,30 @@ public class Activity {
             System.out.println("2. Cosine (cos)");
             System.out.println("3. Tangent (tan)");
 
-            int t = bogo.nextInt();
+            int en = input.nextInt();
 
-            switch (t) {
+            switch (en) {
 
                 case 1:
                     System.out.println("Enter value in degree:");
-                    double sin = bogo.nextDouble();
+                    double sin = input.nextDouble();
                     System.out.println("The sine of" + " " + sin + " " + "is:" + " " + Math.sin(Math.toRadians(sin)));
                     break;
 
                 case 2:
                     System.out.println("Enter value in degree:");
-                    double cos = bogo.nextDouble();
+                    double cos = input.nextDouble();
                     System.out.println("The cosine of" + " " + cos + " " + "is:" + " " + Math.cos(Math.toRadians(cos)));
                     break;
 
                 case 3:
                     System.out.println("Enter value in degree:");
-                    double tan = bogo.nextDouble();
+                    double tan = input.nextDouble();
                     System.out.println("The tangent of" + " " + tan + " " + "is:" + " " + Math.tan(Math.toRadians(tan)));
                     break;
             }
 
-        } else if (hehe == 4) {
+        } else if (jho == 4) {
 
             System.out.println("Please choose a polygon:");
             System.out.println("  ");
@@ -130,18 +128,18 @@ public class Activity {
             System.out.println("2. Square");
             System.out.println("3. Rectangle");
             System.out.println("4. Circle");
-            int p = bogo.nextInt();
+            int ab = input.nextInt();
 
-            switch (p) {
+            switch (ab) {
 
                 case 1:
                     System.out.println("Enter the value of three sides of triangle");
                     System.out.println("Side 1:");
-                    double a = bogo.nextDouble();
+                    double a = input.nextDouble();
                     System.out.println("Side 2:");
-                    double b = bogo.nextDouble();
+                    double b = input.nextDouble();
                     System.out.println("Side 3:");
-                    double c = bogo.nextDouble();
+                    double c = input.nextDouble();
 
                     double abc = ((a + b + c) / 2);
                     double fv = Math.sqrt(abc * (abc - a) * (abc - b) * (abc - c));
@@ -151,7 +149,7 @@ public class Activity {
 
                 case 2:
                     System.out.println("Enter the value of length of the square:");
-                    double d = bogo.nextDouble();
+                    double d = input.nextDouble();
 
                     double fv2 = (d * d);
 
@@ -161,9 +159,9 @@ public class Activity {
                 case 3:
                     System.out.println("Enter the value of length and width of a rectangle");
                     System.out.println("Length:");
-                    double e = bogo.nextDouble();
+                    double e = input.nextDouble();
                     System.out.println("Width");
-                    double f = bogo.nextDouble();
+                    double f = input.nextDouble();
 
                     double fv3 = (e * f);
 
@@ -171,7 +169,7 @@ public class Activity {
 
                 case 4:
                     System.out.println("Enter the value of radius of the circle:");
-                    double g = bogo.nextDouble();
+                    double g = input.nextDouble();
 
                     double fv4 = (Math.PI * g * g);
 
@@ -182,4 +180,3 @@ public class Activity {
         }
     
 }
-
